224ecad9e62c958b915f65c662448f6acdb818fe
Kolozuz <moralesjuanpablo1122@gmail.com>
2023-09-29 12:14:29 -0500

    Cat Upgraded

--- rust/Rust-Huevo/src/main.rs
+++ rust/Rust-Huevo/src/main.rs
@@ -1,32 +1,70 @@
 use std::{thread, time};
 fn main() {
     let frame1 = r#"
-      /\_/\
-     / @ @ \
-    ( >    <)
-     '>>-<<'
-      / o \
+        /\_/\
+       / o o \
+       \¨ ^ ¨/
+        /   \   \
+       /|_|_|\__/
     "#;
     let frame2 = r#"
-      /\_/\
-     / o o \
-    ( >    <)
-     '>>-<<'
-      / o \
+        /\_/\
+       / = = \
+       \¨ ^ ¨/
+        /   \    _
+       /|_|_|\__/
     "#;
+    let frame3 = r#"
+        /\_/\
+       / o o \
+       \¨ ^ ¨/
+        /   \    _
+       /|_|_|\__/
+    "#;
+
+    let frame4 = r#"
+        /\_/\
+       / = = \
+       \¨ w ¨/
+        /   \   \
+       /|_|_|\__/
+"#;
+    let frame5 = r#"
+        /\_/\
+       / o o \
+       \¨ w ¨/
+        /   \    _
+       /|_|_|\__/
+"#;
+
+    let msg = "miau";
     let millis: u64 = 500;
 
     loop {
         print!("{}", frame1);
+        println!("\n{} .", msg);
         thread::sleep(time::Duration::from_millis(millis));
-        clear.terminal();
+        clear_terminal();
         print!("{}", frame2);
+        println!("\n{} ..", msg);
+        thread::sleep(time::Duration::from_millis(millis));
+        clear_terminal();
+        print!("{}", frame3);
+        println!("\n{} ...", msg);
+        thread::sleep(time::Duration::from_millis(millis));
+        clear_terminal();
+        print!("{}", frame4);
+        println!("\n{} ..", msg);
+        thread::sleep(time::Duration::from_millis(millis));
+        clear_terminal();
+        print!("{}", frame5);
+        println!("\n{} .", msg);
         thread::sleep(time::Duration::from_millis(millis));
         clear_terminal();
     }
-    println!("Hello, world!");
+    // println!("Hello, world!");
 }
 
 fn clear_terminal() {
-    print!(\x18[2J\x18[1;1H");
+    print!("\x1B[2J\x1B[1;1H");
 }
