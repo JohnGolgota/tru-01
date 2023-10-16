6726c9d84d26f3e07927bc95d095974daca9ef0c
JohnGolgota <js684new@gmail.com>
2023-10-15 19:50:53 -0500

    wsl config

--- .configuration/windows-setPersonalConfig.ps1
+++ .configuration/windows-setPersonalConfig.ps1
@@ -1,22 +1,53 @@
 # WSL2 https://docs.microsoft.com/en-us/windows/wsl/install-win10
 wsl --install -d Debian
 wsl --set-default-version 2
+wsl {
 	# Dependecies wsl
 	sudo apt update
 	sudo apt upgrade -y
-	# Dependecies zip
-	sudo apt install -y curl wget git zsh
-	git config --global user.name "JohnGolgota"
-	git config --global user.email ""
-	sudo apt install -y zip unzip gzip tar
-	# Dependecies node
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-		nvm install --lts
-		curl -fsSL https://get.pnpm.io/install.sh | sh -
+	# Dependecies avaliable with apt
+	sudo apt install -y curl wget git zsh zip unzip gzip tar
 	# tools
-	sudo apt install -y tmux nvim # nvim Copilot https://github.com/github/copilot.vim
+	sudo apt install -y tmux
+	# Dependecies neovim
+	git config --global user.name "JohnGolgota"
+	git config --global user.email "js684new@gmail.com"
+	nvm & pnpm { # Dependecies node https://github.com/nvm-sh/nvm#installing-and-updating
+		curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+		nvm install --lts
+		# https://pnpm.io/es/installation
+		curl -fsSL https://get.pnpm.io/install.sh | sh -
+	}
+
+	neovim { # https://github.com/neovim/neovim/wiki/Installing-Neovim#linux
+		curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+		chmod u+x nvim.appimage
+		./nvim.appimage
+		# If the ./nvim.appimage command fails, try:
+		err {
+			./nvim.appimage --appimage-extract
+			./squashfs-root/AppRun --version
+
+			# Optional: exposing nvim globally.
+			sudo mv squashfs-root /
+			sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+			nvim
+		}
+		# nvim Copilot https://github.com/github/copilot.vim
+		copilot {
+			git clone https://github.com/github/copilot.vim.git \
+			~/.config/nvim/pack/github/start/copilot.vim
+			{
+				:Copilot setup
+			}
+		}
+	}
+	profilings {
+		nvim ~/.bash_aliases | nvim ~/.zshrc | nvim ~/.bashrc
+	}
 	# Configs vscode https://code.visualstudio.com/docs/remote/wsl
-	
+}
+
 
 
 # Microsoft.VisualStudio.Community
